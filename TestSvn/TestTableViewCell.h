a1653b708f36a320590e06ea90e95dd4f0b86498
123 <a123@123demacbook-pro.local>
2016-06-01 17:37:10 +0800

    d

---
+++ TestSvn/TestTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewCell.h
+//  TestSvn
+//
+//  Created by 123 on 16/6/1.
+//  Copyright © 2016年 昶运网络. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewCell : UITableViewCell
+
+@end
---
+++ TestSvn/TestTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TestTableViewCell.m
+//  TestSvn
+//
+//  Created by 123 on 16/6/1.
+//  Copyright © 2016年 昶运网络. All rights reserved.
+//
+
+#import "TestTableViewCell.h"
+
+@implementation TestTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
